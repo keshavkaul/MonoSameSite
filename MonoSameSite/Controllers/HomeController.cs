d060ae640a15db6bace0d4f1013587dd5f233562
keshav <keshav@keshav>
2024-11-13 21:22:25 +0530

    Initial Commit

---
+++ MonoSameSite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonoSameSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            SetCookie();
+            return View();
+        }
+
+        private void SetCookie()
+        {
+            var cookie = new HttpCookie("SampleCookie", "Hello World")
+            {
+                Expires = DateTime.Now.AddMinutes(1),
+                Secure = true,
+                SameSite = SameSiteMode.None
+            };
+            Response.Cookies.Add(cookie);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
---
+++ MonoSameSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MonoSameSite.MvcApplication" Language="C#" %>
